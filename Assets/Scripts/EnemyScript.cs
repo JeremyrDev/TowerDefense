cbf99023c5587ac67485c80208c7b10183801bd3
JeremyrDev <Jeremyr.dev@gmail.com>
2014-10-29 00:28:12 -0400

    Initial Setup

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	Vector3 endPos;
+	float speed = 5;
+	int wayPointCounter = 0;
+
+	public float health = 100;
+	public bool target = false;
+	float spawnTimer = 0;
+	public float maxHealth = 100;
+	public Material enemy;
+	public Material enemyTarget;
+	void OnEnable()
+	{
+		wayPointCounter = 0;
+		endPos = new Vector3(3.6f, 1.77f, -3.20f);
+		health = maxHealth;
+	}
+	void Start () 
+	{
+		endPos = new Vector3(3.6f, 1.77f, -3.20f);
+	}
+	public void targeted(bool t)
+	{
+		target = t;
+	}
+	public void ApplyDamage(float damage)
+	{
+		Debug.Log("DAMAGE WORKED");
+		health-=damage;
+	}
+	void Update () 
+	{
+		if(target)
+			renderer.material = enemyTarget;
+		else
+			renderer.material = enemy;
+		if(health <=0)
+			gameObject.SetActive(false);
+//		var fwd = transform.TransformDirection (Vector3.forward);
+//		RaycastHit hit;
+//		if (Physics.Raycast (transform.position, fwd, 10)) {
+//			print (hit.point.ToString());
+//
+//		}
+//		Debug.DrawRay(transform.position, fwd, Color.green);
+
+		transform.position = Vector3.MoveTowards(transform.position, endPos, Time.deltaTime * speed);
+		if(wayPointCounter == 0){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(3.6f, 1.77f, 1.20f);
+			}
+		}
+		if(wayPointCounter == 1){
+			if(transform.position == endPos)
+			{
+				wayPointCounter++;
+				endPos = new Vector3(14.5f, 1.77f, 1.20f);
+			}
+		}
+		if(wayPointCounter == 2){
+			if(transform.position == endPos)
+			{
+				gameObject.SetActive(false);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SceneController : MonoBehaviour {
+
+//	public GameObject b;
+//	List<GameObject> bullets;
+
+	public GameObject o;
+	List<GameObject> objects;
+
+	public GameObject t;
+	List<GameObject> towers;
+
+	public GameObject towerLockObject;
+	public GameObject tower;
+
+	float radius = 1000;
+	float spawnTimer = 0;
+	float timerLimit = 1;
+	float distance = 0;
+	float shootTimer = 0;
+
+	int cycleCounter = 0;
+	int objectNumber = 0;
+	int maxHealth = 100;
+	int[] objectHealth;
+
+	string info;
+
+	public GUIStyle infoStyle;
+
+	public Material enemy;
+	public Material enemyTarget;
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (0,0,100,50), info, infoStyle);
+		//GUI.Label(new Rect(0, 0, Screen.width,0), blockCounterTotal.ToString(), scoreStyle);
+		//GUI.Label(new Rect(0, 0, Screen.width, 0),milisecondString, timeStyle);
+	}
+
+	void Start () {
+		objectHealth = new int[20];
+		objects = new List<GameObject>();
+
+		for(int i = 0; i<20; i++)
+		{
+			GameObject obj = (GameObject)Instantiate(o);
+			obj.SetActive(false);
+			objects.Add(obj);
+			objectHealth[i] = 100; 
+		}
+		towerLockObject = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		info = spawnTimer.ToString ();
+		spawnTimer+=1*Time.deltaTime;
+		if(spawnTimer > timerLimit)
+		{
+			spawnTimer = 0;
+			cycleCounter++;
+			if(cycleCounter < 8)
+			{
+				Spawn();
+			}
+			else
+			{
+				towerLockObject = null;
+				if(timerLimit == 3)
+				{
+					timerLimit = 1;
+					cycleCounter = 0;
+				}
+				else
+					timerLimit = 3;
+			}
+		}
+		//Debug.Log (Vector3.Distance (objects[0].transform.position, tower.transform.position));
+		radius = 50;
+		for(int i = 0; i < 20;i++)
+		{
+			if(objects[i].activeInHierarchy)
+			{
+				objects[i].renderer.material = enemy;
+				float temp = Vector3.Distance (objects[i].transform.position, tower.transform.position);
+				//Debug.Log(temp.ToString());
+				if(temp < radius)
+				{
+					radius = temp;
+					objectNumber = i;
+					distance = temp;
+					//Debug.Log("LOWEST: " + temp.ToString() + "OBJECT: "+objectNumber);
+				}
+			}
+		}
+		if(distance <6)
+		{
+			towerLockObject = objects[objectNumber];
+			//objects[objectNumber].renderer.material = enemyTarget;
+			//Debug.Log (objectHealth[objectNumber].ToString());
+			shootTimer+=1*Time.deltaTime;
+			if(shootTimer > .05f){
+				objectHealth[objectNumber] -= 1;
+				shootTimer = 0;
+			}
+			if(objectHealth[objectNumber] <= 0)
+				objects[objectNumber].SetActive(false);
+		}
+		else
+		{
+			towerLockObject = null;
+		}
+	}
+	public void Spawn()
+	{
+		for(int i = 0; i < 20;i++)
+		{
+			if(!objects[i].activeInHierarchy)
+			{
+				objects[i].transform.position = new Vector3(-7.2f, 1.77f, -3.20f);
+				objectHealth[i] = 100;
+				objects[i].SetActive(true);
+				return;
+			}
+		}
+	}
+}
